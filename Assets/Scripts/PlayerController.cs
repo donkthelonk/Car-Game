4eb27f853a53aa7d0cb766df89400c6c9d37fd2c
Robert Giles <robert.ryan.giles@gmail.com>
2023-01-14 15:39:37 -0600

    changed movement from transform.Translate to rigidbody.AddForce and added L/R Walls to environment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float verticalInput;
     public float speed = 10.0f;
+
+    private Rigidbody playerRb;
+
     public float xRange = 15;
     public float zMax = 10;
     public float zMin = -2;
@@ -14,18 +17,19 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Keep the player in bounds 
-        if (transform.position.x < -xRange)
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        //Keep the player in bounds
+        // replaced the x bounds with physical walls
+        //if (transform.position.x < -xRange)
+        //    transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
 
-        if (transform.position.x > xRange)
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        //if (transform.position.x > xRange)
+        //    transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
 
         if (transform.position.z > zMax)
             transform.position = new Vector3(transform.position.x, transform.position.y, zMax);
@@ -33,12 +37,11 @@ public class PlayerController : MonoBehaviour
         if (transform.position.z < zMin)
             transform.position = new Vector3(transform.position.x, transform.position.y, zMin);
 
-        // Get input from user and move player
+        // Get input from user move player
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-
         verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
 
     }
 }
