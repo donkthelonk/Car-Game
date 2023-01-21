66aad8d7cbd6d954aa221a8077cad8b50be91dde
Robert Giles <robert.ryan.giles@gmail.com>
2023-01-21 13:10:17 -0600

    added SpawnTraffic and SpawnPowerup methods to SpawnManager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] traffic;
+    public GameObject powerup;
+
+    private float zTrafficSpawn = 15.0f;
+    private float zPowerupSpawn = 15.0f;
+    private float xSpawnRange = 10.0f;
+    //private float zPowerupRange = 5.0f;   // only used if powerup spawns in the road
+    private float yTrafficSpawn = 0.5f;
+    private float yPowerupSpawn = 0.25f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnTraffic();
+        SpawnPowerup();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Spawn random traffic at top of screen
+    void SpawnTraffic()
+    {
+        // Create random spawnPos x value
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+
+        // Create random array index
+        int randomIndex = Random.Range(0, traffic.Length);
+
+        // Create vector3 for spawnPos
+        Vector3 spawnPos = new Vector3(randomX, yTrafficSpawn, zTrafficSpawn);
+
+        // Spawn random traffic at top of screen
+        Instantiate(traffic[randomIndex], spawnPos, traffic[randomIndex].gameObject.transform.rotation);
+    }
+
+    // Spawn powerup at top of screen
+    void SpawnPowerup()
+    {
+        // Create random spawnPos x value
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+
+        // Create vector3 for spawnPos
+        Vector3 spawnPos = new Vector3(randomX, yPowerupSpawn, zPowerupSpawn);
+
+        // Spawn powerup at top of screen
+        Instantiate(powerup, spawnPos, powerup.transform.rotation);
     }
 }
