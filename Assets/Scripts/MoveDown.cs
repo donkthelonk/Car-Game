3233b8f5cde42c123cc9fb18227656e898963542
Robert Giles <robert.ryan.giles@gmail.com>
2023-01-21 10:39:08 -0600

    added code to MoveDown to destroy objects off screen

--- Assets/Scripts/MoveDown.cs
+++ Assets/Scripts/MoveDown.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MoveDown : MonoBehaviour
 {
     public float speed = 5.0f;
+
+    private float zDestroy = -10.0f;
     private Rigidbody objectRb;
 
     // Start is called before the first frame update
@@ -16,6 +18,13 @@ public class MoveDown : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Move object down the screen
         objectRb.AddForce(Vector3.forward * -speed);
+
+        // Destroy object when it goes off screen
+        if (transform.position.z < zDestroy)
+        {
+            Destroy(gameObject);
+        }
     }
 }
