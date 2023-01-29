626454b6e4b219e9db3de53b1112f0fb5bcfb54d
Robert Giles <robert.ryan.giles@gmail.com>
2023-01-28 21:23:20 -0600

    added Timer script and created timer on screen

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timeLeft;
+    public bool timerOn = false;
+    public TextMeshProUGUI timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerOn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerOn)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                updateTimer(timeLeft);
+            }
+            else
+            {
+                Debug.Log("Time is Up!");
+                timeLeft = 0;
+                timerOn = false;
+            }
+        }
+    }
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        //timerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+        timerText.text = "Timer: " + seconds;
+    }
+}
